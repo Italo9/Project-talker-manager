121c543d10ca9d896832b3ed2eab3c23233cad27
ítalo Lima <contatoitallo@hotmail.com>
2022-06-20 18:48:35 -0300

    feat: Implementa o endpoint GET que retorna um palestrante pelo id, se este existir, caso contrário, mensagem de ERRO -REQ 02

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -7,8 +7,19 @@ const PATH_FILE = './talker.json';
 
 router.get('/', async (_req, res) => {
     const talker = await readContentFile(PATH_FILE) || [];
-  
     res.status(200).json(talker);
   });
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const talker = await readContentFile(PATH_FILE) || [];
+    const validate = talker.find((person) => person.id === Number(id));
+    if (!validate) {
+        return res.status(404).json({
+            message: 'Pessoa palestrante não encontrada',
+          });
+    }
+    return res.status(200).json(validate);
+});
 
 module.exports = router;
