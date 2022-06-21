af5b1c656a328fff884c9e1d12b96e592852ef45
ítalo Lima <contatoitallo@hotmail.com>
2022-06-21 15:12:29 -0300

    fix: corrige segundo IF da função isValidEmail

--- helpers/automaticToken.js
+++ helpers/automaticToken.js
@@ -1,8 +1,10 @@
 const crypto = require('crypto');
 
 const automaticToken = () => {
+    // if (password && email) {
     const token = crypto.randomBytes(8).toString('hex');
     return token;
+    // }
 };
 
 module.exports = automaticToken;
--- middlewares/validations.js
+++ middlewares/validations.js
@@ -1,17 +1,3 @@
-const isValidEmail = (req, res, next) => {
-    const { email } = req.body;
-    if (!email) {
-        return res.status(400).json({
-          message: 'O campo "email" é obrigatório',
-        });
-    } if (!email.includes('@') || email.includes('.com')) {
-      return res.status(400).json({
-        message: 'O "email" deve ter o formato "email@email.com"',
-      });
-    }
-    next();
-};
-
 const isValidPassword = (req, res, next) => {
     const { password } = req.body;
     if (!password) {
@@ -23,6 +9,20 @@ const isValidPassword = (req, res, next) => {
       message: 'O "password" deve ter pelo menos 6 caracteres',
     });
   }
+  next();
+};
+
+const isValidEmail = (req, res, next) => {
+  const { email } = req.body;
+  if (!email) {
+      return res.status(400).json({
+        message: 'O campo "email" é obrigatório',
+      });
+  } if (!email.includes('@') || !email.includes('.com')) {
+    return res.status(400).json({
+      message: 'O "email" deve ter o formato "email@email.com"',
+    });
+  }     
   next();
 };
 
