ad36fedf683373241e7072bc6bd270aed26d7ec0
ítalo Lima <contatoitallo@hotmail.com>
2022-06-22 16:13:01 -0300

    feat: Implementa o endpoint PUT /talker/:id para editar uma pessoa palestrante com base no id da rota- REQ 06

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -99,15 +99,15 @@ const authTalkAdditional = (req, res, next) => {
       message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
     });
   }
-  if (!talk.rate) {
-    return res.status(400).json({
-      message: 'O campo "rate" é obrigatório',
-    }); 
-  }
-  if (talk.rate > 5 || talk.rate < 1) {
+    if (talk.rate < 1 || talk.rate > 5) {
     return res.status(400).json({
       message: 'O campo "rate" deve ser um inteiro de 1 à 5',
     });
+}
+  if (!talk.rate) {
+  return res.status(400).json({
+    message: 'O campo "rate" é obrigatório',
+  }); 
 }
   next(); 
 };
--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -30,13 +30,21 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', authToken, authName, authAge, authTalk, authTalkAdditional, async (req, res) => {
   const talker = req.body;
-
   const talkers = await readContentFile(PATH_FILE) || [];
-  // talkers.push({ id: talkers.length + 1, ...talker });
-  // const newTalker = await readContentFile(PATH_FILE, JSON.stringify(talkers));
-  // const addTalker = [{ ...talkers, talker }];
-  const newTalker = await writeContentFile(PATH_FILE, { id: talkers.length + 1, ...talker });
-  return res.status(201).json(newTalker);
+  talkers.push({ id: talkers.length + 1, ...talker });
+  await writeContentFile(PATH_FILE, talkers);
+  return res.status(201).json(talkers[talkers.length - 1]);
+});
+
+router.put('/:id', authToken, authName, authAge, authTalk, authTalkAdditional, async (req, res) => {
+  const { id } = req.params;
+  const talkerEdit = req.body;
+  const talkers = await readContentFile(PATH_FILE) || [];
+  const editTalker = talkers.filter((talker) => (talker.id !== Number(id)));
+  editTalker.push({ id: Number(id), ...talkerEdit });
+  await writeContentFile(PATH_FILE, editTalker);
+  console.log('editTalker', editTalker);
+  return res.status(200).json(editTalker[editTalker.length - 1]);
 });
 
 module.exports = router;
